5e16514602726f3171705f37e48eee0439873bf3
Alexsander <alexsander1rj@gmail.com>
2019-06-23 11:53:40 -0300

    Tratamento data e Hora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,59 @@
 class CalcController {
-  
- 
 
+    constructor(){
+        this._locale = "pt-BR";
+        this._displayEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
 
+        this.initialaze();  
+    }
+
+    initialaze(){
+
+        this.setDisplayDateTime();
+
+        setInterval(()=>{
+            this.setDisplayDateTime();            
+        }, 1000);       
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayCalc(){
+        return this._displayEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        return this._currentDate = value;
+    }
 }
